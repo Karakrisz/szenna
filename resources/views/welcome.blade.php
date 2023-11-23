109f827ad6a348e98bfabc464165310081ea575d
Boaz <webevolution13@gmail.com>
2023-11-23 12:54:01 +0100

    upDate
    front end

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,6 +77,107 @@
                         <strong class="menu-content__text-box__div__text-box__strong">2950 Ft</strong>
                     </div>
 
+                    <h6 class="menu-content__text-box__div__h6 text-transform-uppercase">VADÉTEL</h6>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Szarvassült érmék narancsmártással</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">1450 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Zselici szarvas sült zöldborsó mártással</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">2950 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p"> Sült vaddisznó barnamártással</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">1450 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Szennai tűzdelt vaddisznó érmék
+                            rozmaringmártással</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">2950 Ft</strong>
+                    </div>
+
+                    <h6 class="menu-content__text-box__div__h6 text-transform-uppercase">MARHÁBÓL KÉSZÜLT ÉTEL</h6>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Vörösboros marhapörkölt galuskával</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">4450 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Ribeye steak</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">5950 Ft</strong>
+                    </div>
+
+                    <h6 class="menu-content__text-box__div__h6 text-transform-uppercase">TÁL</h6>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">2 személyes tál</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">3450 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Vadásztál</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">3950 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Szennai tál</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">3950 Ft</strong>
+                    </div>
+
+                    <h6 class="menu-content__text-box__div__h6 text-transform-uppercase">HALÉTEL</h6>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Lazacfilé citrommártással</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">3450 Ft</strong>
+                    </div>
+
+                    <h6 class="menu-content__text-box__div__h6 text-transform-uppercase">VEGA</h6>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Vega menü</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">1450 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Rántott Camembert áfonyalekvárral</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">2950 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">HAMBURGER</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">2700 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Retro hamburger</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">1450 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Fekete extra burger</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">2950 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Cheddar burger</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">2700 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Dupla húsos cheddar burger</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">1450 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Mérges burger</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">2950 Ft</strong>
+                    </div>
+
                 </div>
 
             </div>
@@ -166,6 +267,97 @@
                         <p class="menu-content__text-box__div__text-box__p">Sajtos tészta</p>
                         <strong class="menu-content__text-box__div__text-box__strong">2950 Ft</strong>
                     </div>
+
+                    <h6 class="menu-content__text-box__div__h6 text-transform-uppercase">GYROS</h6>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Gyros tál</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">1450 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Dupla húsos gyros tál</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">2950 Ft</strong>
+                    </div>
+
+                    <h6 class="menu-content__text-box__div__h6 text-transform-uppercase">KÖRET</h6>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Jázmin rizs</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">950 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Galuska</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">1950 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Hasábburgonya</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">950 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Burgonyakrokett</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">1950 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Grillezett zöldségek</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">950 Ft</strong>
+                    </div>
+
+                    <h6 class="menu-content__text-box__div__h6 text-transform-uppercase">SALÁTA</h6>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Friss saláta</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">950 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Csirkesaláta</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">1950 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Görög saláta</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">950 Ft</strong>
+                    </div>
+
+                    <h6 class="menu-content__text-box__div__h6 text-transform-uppercase">SAVANYÚSÁG</h6>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Káposztasaláta</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">950 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Csemege uborka</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">1950 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Csípős almapaprika</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">950 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Vegyes savanyúság</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">1950 Ft</strong>
+                    </div>
+
+                    <h6 class="menu-content__text-box__div__h6 text-transform-uppercase">DESSZERT</h6>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Fagylaltkehely</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">950 Ft</strong>
+                    </div>
+
+                    <div class="menu-content__text-box__div__text-box d-flex">
+                        <p class="menu-content__text-box__div__text-box__p">Csoki brownie vaníliás fagylalttal</p>
+                        <strong class="menu-content__text-box__div__text-box__strong">1950 Ft</strong>
+                    </div>
+
                 </div>
             </div>
 
